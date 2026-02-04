5359bb1b7d6ec132815bf1e3b59aa732cb98ca1f
VittorioRossetto <rossetto.vittorio2108@gmail.com>
2026-02-04 16:19:33 +1100

    Corrected chap1, written first section chap4

--- scripts/run_feature_configs.sh
+++ scripts/run_feature_configs.sh
@@ -56,9 +56,9 @@ declare -a CONFIGS=(
 
 # Temperature sweep for each configuration (passed through to benchmark_chat.py)
 declare -a TEMPS=(
-  "0.2"
-  "0.0"
-  "0.8"
+  # "0.2"
+  # "0.0"
+  # "0.8"
   "0.3"
   "0.7"
 )
